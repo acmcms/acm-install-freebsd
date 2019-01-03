8cee8c0c7a589725542d409c6daf78660b1937ff
vlapan <vlapan@vlapan.com>
2019-01-03 14:46:43 +0300

    stage2 init finished

--- host/tarball/acmbsd/scripts/instance.sh
+++ host/tarball/acmbsd/scripts/instance.sh
@@ -216,7 +216,7 @@ THIS.start() {
 						fi
 					fi
 				done
-				if [ "`cat $THIS_OUT | fgrep 'stage2 init finished'`" ]; then
+				if [ "`cat $THIS_OUT | fgrep 'Stage2 Init Finished.'`" ]; then
 					out.status green DONE
 					break;
 				fi
