f845a22945cda6e7be1c71b85241126f9aad1e45
vlapan <vlapan@vlapan.com>
2019-01-03 04:29:27 +0300

    install postgresql 11 by default

--- host/tarball/acmbsd/scripts/pgsql.sh
+++ host/tarball/acmbsd/scripts/pgsql.sh
@@ -9,7 +9,7 @@ pgsql.check() {
 	# base.file.checkLine /etc/sysctl.conf kern.ipc.shmmax kern.ipc.shmmax=134217728
 	base.file.checkLine /etc/sysctl.conf kern.ipc.shm_use_phys kern.ipc.shm_use_phys=1
 
-	pkg.install postgresql*-server databases/postgresql10-server
+	pkg.install postgresql*-server databases/postgresql11-server
 
 	chown -R pgsql:pgsql /usr/local/share/postgresql
 	chown -R pgsql:pgsql /usr/local/lib/postgresql
