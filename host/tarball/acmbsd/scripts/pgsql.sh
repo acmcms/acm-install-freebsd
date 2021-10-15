2d2339efb90a330e9f4fe9ba055fb647d4c548ab
vlapan <vlapan@vlapan.com>
2021-10-15 16:27:56 +0300

    fix

--- host/tarball/acmbsd/scripts/pgsql.sh
+++ host/tarball/acmbsd/scripts/pgsql.sh
@@ -9,7 +9,7 @@ pgsql.check() {
 	# base.file.checkLine /etc/sysctl.conf kern.ipc.shmmax kern.ipc.shmmax=134217728
 	base.file.checkLine /etc/sysctl.conf kern.ipc.shm_use_phys kern.ipc.shm_use_phys=1
 
-	pkg.install postgresql*-server databases/postgresql11-server
+	pkg.install postgresql*-server databases/postgresql14-server
 
 	chown -R ${PGROOTUSER}:${PGROOTUSER} /usr/local/share/postgresql
 	chown -R ${PGROOTUSER}:${PGROOTUSER} /usr/local/lib/postgresql
